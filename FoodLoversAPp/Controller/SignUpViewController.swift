f77659c444b0d45edef3e2b2a388643cfdea3582
Thobio Joseph <love2code@icloud.com>
2018-08-25 17:03:27 +0530

    SiginUp UI design

---
+++ FoodLoversAPp/Controller/SignUpViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SignUpViewController.swift
+//  FoodLoversAPp
+//
+//  Created by Thobio Joseph on 25/08/18.
+//  Copyright © 2018 Love2Code. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func signinClose(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
