03887889ddfb2a4344a1437cb6f69e64feb6a03b
Thobio Joseph <love2code@icloud.com>
2018-08-26 15:19:02 +0530

    Sigin in TextFields UI Design

--- FoodLoversAPp/Controller/SigninViewController.swift
+++ FoodLoversAPp/Controller/SigninViewController.swift
@@ -13,14 +13,27 @@ class SigninViewController: UIViewController {
     @IBOutlet var passwordTextFields: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        UIDisgnFunction()
     }
     func UIDisgnFunction(){
-        self.emailTextFields.backgroundColor = UIColor.clear
-        self.emailTextFields.attributedPlaceholder = NSAttributedString(string: emailTextFields.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: UIColor(white: 1.0, alpha: 0.6)])
         
-        self.passwordTextFields.backgroundColor = UIColor.clear 
+        self.emailTextFields.attributedPlaceholder = NSAttributedString(string: emailTextFields.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: UIColor(white: 1.0, alpha: 0.6)])
+        self.emailTextFields.layer.addSublayer(textFieldsDesign(textFields: self.emailTextFields))
+        
+        
         self.passwordTextFields.attributedPlaceholder = NSAttributedString(string: passwordTextFields.placeholder!, attributes: [NSAttributedStringKey.foregroundColor : UIColor(white: 1.0, alpha: 0.6)])
+        self.passwordTextFields.layer.addSublayer(textFieldsDesign(textFields: self.passwordTextFields))
+        
     }
+    
+    func textFieldsDesign(textFields:UITextField) -> CALayer{
+        
+        let bottomLayer = CALayer()
+        bottomLayer.frame = CGRect(x: 0, y: textFields.bounds.height - 1, width: textFields.bounds.width + 8, height: 0.6)
+        bottomLayer.backgroundColor = UIColor.white.withAlphaComponent(0.4).cgColor
+        return bottomLayer
+        
+    }
+    
 }
 
